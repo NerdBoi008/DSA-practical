13ba46f5ba0b9737128de9c9509cb9b0f5803062
nerdBoi <96974553+NerdBoi008@users.noreply.github.com>
2022-01-13 14:45:57 +0530

    Add files via upload

---
+++ DSA practcal/DSA_prac_3_2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void binarySearch(int arr[], int len, int value)
+{
+	int right = len, left = 0, mid, flag = 0,count = 0;
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		if(arr[mid] == value)
+		{
+			flag = 1;
+			break;
+		}
+		if(arr[mid] > value)
+		{
+			right = mid -1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	(flag == 1) ? printf("[+] value found.") : printf("[-] value not found.");
+}
+
+int main(void)
+{
+	int value;
+	int arr[10] = {2,5,8,13,15,22,25,27,30,37};
+	printf("Enter Value to find: ");
+	scanf("%d",&value);
+
+	binarySearch(arr, 10, value);
+	return 0;
+}
